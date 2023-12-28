8f53d9f0be0d86090d8c8cec22886c2112b875da
Darley <133307393+silvadarley@users.noreply.github.com>
2023-12-27 23:00:29 -0300

    Implementando useState na apresentação das informações do projeto

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,11 +1,25 @@
 import styles from './Card.module.css';
 import ButtonB from './ButtonB';
+import { useState } from 'react';
+
 
 
 function Card({imagem, title, site, tecnologia, descricao, repositorio}){
+
+    const [info, setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+
+    function infoOff(){
+        setInfo(false)
+    }
+
     return(
-        <div className={styles.card}>
+        <div onMouseLeave={infoOff} className={styles.card}>
             <a 
+                onMouseEnter={InfoOn}
                 target='_blank' 
                 rel="noreferrer" 
                 href={site}
@@ -13,26 +27,30 @@ function Card({imagem, title, site, tecnologia, descricao, repositorio}){
                 <img src={imagem} alt='Error'></img>
             </a>
 
-            <section>
-                <h2>Titulo: {title}</h2>
-                <p>
-                    Tecnologia: 
-                    <br/>
-                    {tecnologia}
-                </p>
+            {info === true &&
+                (
+                    <section>
+                        <h2>Titulo: {title}</h2>
+                        <p>
+                            Tecnologia: 
+                            <br/>
+                            {tecnologia}
+                        </p>
 
-                <p> 
-                    Descrição:
-                    <br/>
-                    {descricao}
-                </p>
+                        <p> 
+                            Descrição:
+                            <br/>
+                            {descricao}
+                        </p>
 
-                <ButtonB 
-                    text='Acesse meu projeto aqui!' 
-                    link={repositorio}
-                />
+                        <ButtonB 
+                            text='Acesse meu projeto aqui!' 
+                            link={repositorio}
+                        />
 
-            </section>
+                    </section>
+                )
+            }
         </div>
     )
 }
